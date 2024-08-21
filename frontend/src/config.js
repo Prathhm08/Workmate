fc32b42e627a78eca9ffa93b713e28af34415458
Pratham <pkrkg25@gmail.com>
2024-08-21 15:52:55 +0530

    changed the config base url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL =
+  "https://workmatebackend-i2dipsru6-prathhm08s-projects.vercel.app/api/v1";
 export const token = localStorage.getItem("token");
